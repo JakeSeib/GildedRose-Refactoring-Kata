2b00620280274e5169d5326c2853c1555966a8dd
JakeSeib <jacob.a.seib@gmail.com>
2020-03-16 17:45:58 -0400

    Add single check for negative quality
    Normal items have only 1 check for negative quality
    Add more syntactic sugar

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -17,12 +17,8 @@ class GildedRose
         # special items
         item.quality += 1
         if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          if item.sell_in < 11
-            item.quality += 1
-          end
-          if item.sell_in < 6
-            item.quality += 1
-          end
+          item.quality += 1 if item.sell_in < 11
+          item.quality += 1 if item.sell_in < 6
         end
         item.sell_in -= 1
         if item.sell_in < 0
@@ -36,15 +32,10 @@ class GildedRose
 
       else
         # normal items
-        if item.quality > 0
-          item.quality -= 1
-          item.sell_in -= 1
-          if item.sell_in < 0
-            if item.quality > 0
-              item.quality -= 1
-            end
-          end
-        end
+        item.quality -= 1
+        item.sell_in -= 1
+        item.quality -= 1 if item.sell_in < 0
+        item.quality = 0 if item.quality < 0
       end
     end
   self
