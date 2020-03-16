fb7e891d0f1d6da766f774221b48b7e5d4044b0b
JakeSeib <jacob.a.seib@gmail.com>
2020-03-16 18:54:36 -0400

    Use class methods for each type of update
    Add class methods to GildedRose for each possible type of update
    Refactor normal item update method

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -9,9 +9,37 @@ class GildedRose
     special_items.include? item.name
   end
 
+  def self.update_special_item(item)
+    GildedRose.update_backstage(item) if item.name == "Backstage passes to a TAFKAL80ETC concert"
+    GildedRose.update_brie(item) if item.name == "Aged Brie"
+  end
+
   def self.validate_quality(item)
     item.quality = 50 if item.quality > 50
-    item.quality = 0 if item.quality < 0
+    item.quality = 0 if item.quality.negative?
+  end
+
+  def self.update_normal_item(item)
+    item.sell_in -= 1
+    item.quality = if item.sell_in.negative?
+                     item.quality - 2
+                   else
+                     item.quality - 1
+                   end
+  end
+
+  def self.update_brie(item)
+    item.quality += 1
+    item.sell_in -= 1
+    item.quality += 1 if item.sell_in.negative?
+  end
+
+  def self.update_backstage(item)
+    item.quality += 1
+    item.quality += 1 if item.sell_in < 11
+    item.quality += 1 if item.sell_in < 6
+    item.sell_in -= 1
+    item.quality = 0 if item.sell_in < 0
   end
 
   def update_quality()
@@ -20,28 +48,15 @@ class GildedRose
 
       if GildedRose.special_item?(item)
         # special items
-
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          item.quality += 1
-          item.quality += 1 if item.sell_in < 11
-          item.quality += 1 if item.sell_in < 6
-          item.sell_in -= 1
-          item.quality = 0 if item.sell_in < 0
-        else
-          item.quality += 1
-          item.sell_in -= 1
-          item.quality += 1 if item.sell_in < 0
-        end
-
+        GildedRose.update_special_item(item)
       else
         # normal items
-        item.quality -= 1
-        item.sell_in -= 1
-        item.quality -= 1 if item.sell_in < 0
+        GildedRose.update_normal_item(item)
       end
+      # ensure quality between 0 and 50
       GildedRose.validate_quality(item)
     end
-  self
+    self
   end
 end
 
